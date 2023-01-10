f7e5cd41689205d37f6d35e7d57bfe1e49ecb26e
DavidCitron <zizlavsky.d@seznam.cz>
2023-01-10 11:26:59 +0100

    commit

--- src/Kolekce.java
+++ src/Kolekce.java
@@ -1,2 +1,37 @@
-package PACKAGE_NAME;public class Kolekce {
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Kolekce {
+    static final String SOUBOR = "dokument.txt";
+    static final String ODDELOVAC = ":";
+    List<Zakaznik> seznamZakazniku = new ArrayList<>();
+    void pridejZakaznika(Zakaznik novyZakaznik){
+        try{
+            PrintWriter writer = new PrintWriter(new BufferedWriter(new FileWriter(SOUBOR)));
+            seznamZakazniku.add(novyZakaznik);
+            for(Zakaznik aktualniZakaznik: seznamZakazniku){
+                writer.println(aktualniZakaznik.getJmeno() + ODDELOVAC + aktualniZakaznik.getPocetProdeju());
+            }
+            writer.flush();
+        } catch (IOException e) {
+            System.err.println(e.getLocalizedMessage());
+        }
+    }
+
+    void nactiSeznam(){
+        try{
+            Scanner scanner = new Scanner(new BufferedReader(new FileReader(SOUBOR)));
+            seznamZakazniku.clear();
+            while(scanner.hasNextLine()){
+                String radek = scanner.nextLine();
+                String[] casti = radek.split(ODDELOVAC);
+                seznamZakazniku.add(new Zakaznik(casti[0], Integer.parseInt(casti[1])));
+            }
+            scanner.close();
+        } catch (FileNotFoundException e) {
+            System.err.println(e.getLocalizedMessage());
+        }
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Kolekce kolekce = new Kolekce();
+        kolekce.nactiSeznam();
+        kolekce.pridejZakaznika(new Zakaznik("Petr Pavel", 69));
     }
 }
