6d8ec5171e7091627bf0953aa5816278780728b0
DavidCitron <91932673+DavidCitron@users.noreply.github.com>
2023-01-10 12:00:27 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ public class Main {
         Kolekce kolekce = new Kolekce();
         kolekce.nactiSeznam();
         kolekce.pridejZakaznika(new Zakaznik("Petr Pavel", 69));
+        kolekce.pridejZakaznika(new Zakaznik("Danuše Nerudová", 5));
         List<Zakaznik> seznam = kolekce.ziskejSeznamZakazniku();
         for(Zakaznik zakaznik: seznam){
             System.out.println(zakaznik.getJmeno() + " - " + zakaznik.getPocetProdeju());
