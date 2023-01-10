87c09f465a90dba7ed3f4fd25dce8fa6704c1097
DavidCitron <zizlavsky.d@seznam.cz>
2023-01-10 11:43:37 +0100

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,13 @@
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         Kolekce kolekce = new Kolekce();
         kolekce.nactiSeznam();
         kolekce.pridejZakaznika(new Zakaznik("Petr Pavel", 69));
+        List<Zakaznik> seznam = kolekce.ziskejSeznamZakazniku();
+        for(Zakaznik zakaznik: seznam){
+            System.out.println(zakaznik.getJmeno() + " - " + zakaznik.getPocetProdeju());
+        }
     }
 }
